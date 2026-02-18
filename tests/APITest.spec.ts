1aae3e933cfaa07af12f359431b1f9aba59a5b0b
benardkamau <benard.kamau@griffinglobaltech.com>
2026-02-18 20:20:14 +0300

    Edit the API file

--- tests/APITest.spec.ts
+++ tests/APITest.spec.ts
@@ -1,102 +1,102 @@
-import { test, expect, request } from '@playwright/test';
+// import { test, expect, request } from '@playwright/test';
 
-test.skip(process.env.CI, 'Skipping this test in CI');
+// test.skip(process.env.CI, 'Skipping this test in CI');
 
 
-const REPO = 'test-repo-1';
-const USER = 'github-username';
+// const REPO = 'test-repo-1';
+// const USER = 'github-username';
 
-test('should create a bug report', async ({ request }) => {
-  const newIssue = await request.post(`/repos/${USER}/${REPO}/issues`, {
-    data: {
-      title: '[Bug] report 1',
-      body: 'Bug description',
-    }
-  });
-  expect(newIssue.ok()).toBeTruthy();
+// test('should create a bug report', async ({ request }) => {
+//   const newIssue = await request.post(`/repos/${USER}/${REPO}/issues`, {
+//     data: {
+//       title: '[Bug] report 1',
+//       body: 'Bug description',
+//     }
+//   });
+//   expect(newIssue.ok()).toBeTruthy();
 
-  const issues = await request.get(`/repos/${USER}/${REPO}/issues`);
-  expect(issues.ok()).toBeTruthy();
-  expect(await issues.json()).toContainEqual(expect.objectContaining({
-    title: '[Bug] report 1',
-    body: 'Bug description'
-  }));
-});
+//   const issues = await request.get(`/repos/${USER}/${REPO}/issues`);
+//   expect(issues.ok()).toBeTruthy();
+//   expect(await issues.json()).toContainEqual(expect.objectContaining({
+//     title: '[Bug] report 1',
+//     body: 'Bug description'
+//   }));
+// });
 
-test('should create a feature request', async ({ request }) => {
-  const newIssue = await request.post(`/repos/${USER}/${REPO}/issues`, {
-    data: {
-      title: '[Feature] request 1',
-      body: 'Feature description',
-    }
-  });
-  expect(newIssue.ok()).toBeTruthy();
+// test('should create a feature request', async ({ request }) => {
+//   const newIssue = await request.post(`/repos/${USER}/${REPO}/issues`, {
+//     data: {
+//       title: '[Feature] request 1',
+//       body: 'Feature description',
+//     }
+//   });
+//   expect(newIssue.ok()).toBeTruthy();
 
-  const issues = await request.get(`/repos/${USER}/${REPO}/issues`);
-  expect(issues.ok()).toBeTruthy();
-  expect(await issues.json()).toContainEqual(expect.objectContaining({
-    title: '[Feature] request 1',
-    body: 'Feature description'
-  }));
-});
+//   const issues = await request.get(`/repos/${USER}/${REPO}/issues`);
+//   expect(issues.ok()).toBeTruthy();
+//   expect(await issues.json()).toContainEqual(expect.objectContaining({
+//     title: '[Feature] request 1',
+//     body: 'Feature description'
+//   }));
+// });
 
-// using beforeAll and afterAll
-test.beforeAll(async ({ request }) => {
-  // Create a new repository
-  const response = await request.post('/user/repos', {
-    data: {
-      name: REPO
-    }
-  });
-  expect(response.ok()).toBeTruthy();
-});
+// // using beforeAll and afterAll
+// test.beforeAll(async ({ request }) => {
+//   // Create a new repository
+//   const response = await request.post('/user/repos', {
+//     data: {
+//       name: REPO
+//     }
+//   });
+//   expect(response.ok()).toBeTruthy();
+// });
 
-test.afterAll(async ({ request }) => {
-  // Delete the repository
-  const response = await request.delete(`/repos/${USER}/${REPO}`);
-  expect(response.ok()).toBeTruthy();
-});
+// test.afterAll(async ({ request }) => {
+//   // Delete the repository
+//   const response = await request.delete(`/repos/${USER}/${REPO}`);
+//   expect(response.ok()).toBeTruthy();
+// });
 
-//using request context
-(async () => {
-  // Create a context that will issue http requests.
-  const context = await request.newContext({
-    baseURL: 'https://api.github.com',
-  });
+// //using request context
+// (async () => {
+//   // Create a context that will issue http requests.
+//   const context = await request.newContext({
+//     baseURL: 'https://api.github.com',
+//   });
 
-  // Create a repository.
-  await context.post('/user/repos', {
-    headers: {
-      'Accept': 'application/vnd.github.v3+json',
-      // Add GitHub personal access token.
-      'Authorization': `token ${process.env.API_TOKEN}`,
-    },
-    data: {
-      name: REPO
-    }
-  });
+//   // Create a repository.
+//   await context.post('/user/repos', {
+//     headers: {
+//       'Accept': 'application/vnd.github.v3+json',
+//       // Add GitHub personal access token.
+//       'Authorization': `token ${process.env.API_TOKEN}`,
+//     },
+//     data: {
+//       name: REPO
+//     }
+//   });
 
-  // Delete a repository.
-  await context.delete(`/repos/${USER}/${REPO}`, {
-    headers: {
-      'Accept': 'application/vnd.github.v3+json',
-      // Add GitHub personal access token.
-      'Authorization': `token ${process.env.API_TOKEN}`,
-    }
-  });
-})();
+//   // Delete a repository.
+//   await context.delete(`/repos/${USER}/${REPO}`, {
+//     headers: {
+//       'Accept': 'application/vnd.github.v3+json',
+//       // Add GitHub personal access token.
+//       'Authorization': `token ${process.env.API_TOKEN}`,
+//     }
+//   });
+// })();
 
-//Reusing authentication state
-const requestContext = await request.newContext({
-  httpCredentials: {
-    username: 'user',
-    password: 'passwd'
-  }
-});
-await requestContext.get(`https://api.example.com/login`);
-// Save storage state into the file.
-await requestContext.storageState({ path: 'state.json' });
+// //Reusing authentication state
+// const requestContext = await request.newContext({
+//   httpCredentials: {
+//     username: 'user',
+//     password: 'passwd'
+//   }
+// });
+// await requestContext.get(`https://api.example.com/login`);
+// // Save storage state into the file.
+// await requestContext.storageState({ path: 'state.json' });
 
-// Create a new context with the saved storage state.
-const context = await browser.newContext({ storageState: 'state.json' });
+// // Create a new context with the saved storage state.
+// const context = await browser.newContext({ storageState: 'state.json' });
 
