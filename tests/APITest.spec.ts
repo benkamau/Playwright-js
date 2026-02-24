c85bf1e548ef710f4daaa383970ecccce2a3e4cc
benardkamau <benard.kamau@griffinglobaltech.com>
2026-02-24 19:18:08 +0300

    Change the Test generator to test-2.spect.ts for record test. Added a sample API test

--- tests/APITest.spec.ts
+++ tests/APITest.spec.ts
@@ -1,6 +1,29 @@
-// import { test, expect, request } from '@playwright/test';
+ import { test, expect, request } from '@playwright/test';
 
-// test.skip(process.env.CI, 'Skipping this test in CI');
+test('gets the json from api and adds a new fruit', async ({ page }) => {
+  // Get the response and add to it
+  await page.route('*/**/api/v1/fruits', async route => {
+    const response = await route.fetch();
+    const json = await response.json();
+    json.push({ name: 'Loquat', id: 100 }, { name: 'Ovacado', id: 101 });
+    // Fulfill using the original response, while patching the response body
+    // with the given JSON object.
+    await route.fulfill({ response, json });
+  });
+
+  // Go to the page
+  await page.goto('https://demo.playwright.dev/api-mocking');
+
+  // Assert that the new fruit is visible
+  await expect(page.getByText('Loquat', { exact: true })).toBeVisible();
+  await expect(page.getByText('Ovacado', { exact: true })).toBeVisible();
+});
+
+test('Sucessful response', async ({ request }) => {
+  const response = await request.get('https://jsonplaceholder.typicode.com/posts/1');
+  expect(response.ok()).toBeTruthy();   
+   
+});
 
 
 // const REPO = 'test-repo-1';
